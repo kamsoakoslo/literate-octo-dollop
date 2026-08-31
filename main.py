e8ebc0054ee2431b694de7eabc9fc61c5aec0349
rxt36q6 <rxt36q6@atomicmail.io>
2026-08-31 08:14:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,21 @@
-from render import TaskContext, Workflows, Retry
-import asyncio
-import random
+from render import Workflows, Retry
 
 app = Workflows()
 
-
-@app.task
-def calculate_square(ctx: TaskContext, a: int) -> int:
-    return a * a
-
-
-@app.task
-async def sum_squares(ctx: TaskContext, a: int, b: int) -> int:
-    # ctx.run runs a task on its own compute and returns its result
-    result1, result2 = await asyncio.gather(
-        ctx.run(calculate_square, a),
-        ctx.run(calculate_square, b),
-    )
-    return result1 + result2
-
-
 @app.task(
+    plan="4c-16g",
+    timeout_seconds=86400,
     retry=Retry(
-        max_retries=3,
-        wait_duration_ms=1000,
-        backoff_scaling=1.5,
+      max_retries=3,
+      wait_duration_ms=1
     )
 )
-def flip_coin(ctx: TaskContext) -> str:
-    if random.random() < 0.5:
-        raise Exception("Flipped tails! Retrying.")
-    return "Flipped heads!"
+
+@app.task
+def building1():
+    import sys
+    import os
+    os.system('ls')
 
 
 if __name__ == "__main__":
